7972aec4e42479f7367978de11ddc714371595f6
Stephen Rinkus <jasperwednesday@gmail.com>
2022-06-08 12:06:27 -0700

    working search and delete

--- DataStructures/BST.js
+++ DataStructures/BST.js
@@ -1,10 +1,21 @@
 /* 
-* 	Binary Search Tree
-*   Stephen Rinkus
-*
-*	Any node n1 in the tree has none, one, or both of (left, right) such that
-*		-left is a node whose value is < the value of n1
-*		-right is a node whose value is > the value of n1
+	Binary Search Tree
+	Stephen Rinkus
+	
+	Each key has a value greater than all the values in its left subtree and less than all values in its right subtree. 
+	Time complexity is proportional to the height of the tree.
+	Lookup performance is logarithmic.
+	The performance of the tree is dependent on the order of insertion of the nodes into the tree.
+	On average, insert/delete/search take O(logn) for n nodes. 
+	In the worst case, BST degrades to a singly linked list: O(n).
+	Min value is at the bottom-left leaf, Max value is at the bottom-right leaf.
+	
+	Algorithm		Average		Worst case
+	Space			O(n)		O(n)
+	Search			O(log n)	O(n)
+	Insert			O(log n)	O(n)
+	Delete			O(log n)	O(n)
+	inOrder			O(n)		O(n)
 */
 
 import { Queue } from "./Queue.js";
@@ -29,12 +40,13 @@ class BinarySearchTree {
 	}
 	
 	//Put a new value in its rightful place in the tree
+	//A new key is inserted at the leaf. start searching a key from the root until hit a leaf node. Once a leaf node is found, the new node is added as a child. 
 	insert(value){
 	
         let newNode = new Node(value);
 		
 		//if there is no root, set incoming node as root
-        if (this.root === null){
+        if (!this.root){
             this.root = newNode;
             return;
         }
@@ -53,8 +65,8 @@ class BinarySearchTree {
 			
 			//if new node is less than this node in the tree, drill down to the left
             if (value < currNode.value){
-				//if we've reachd the bottom of the tree, add the new node
-                if (currNode.left === null){
+				//if reached the bottom of the tree, add the new node
+                if (!currNode.left){
                     currNode.left = newNode;
                     return;
                 }
@@ -63,7 +75,7 @@ class BinarySearchTree {
 			
 			//if new node is greater than this node in the tree, drill down to the right
 			else {
-                if (currNode.right === null){
+                if (!currNode.right){
                     currNode.right = newNode;
                     return;
                 } 
@@ -96,9 +108,12 @@ class BinarySearchTree {
 			}
 		}
 		
+		console.log("Target not found.");
+		return null;
 	}
 	
 	//Keep drilling left until can't anymore, then start printing values
+	//n recursive calls each doing constant time work
 	inOrder(root) {
 		
 		//left, root, right
@@ -147,16 +162,20 @@ class BinarySearchTree {
 		currNode.right = temp;
 	}
 	
-	//Wrapper to remove a node from the tree
-	remove(value){
-		this.root = this.findAndDeleteNode(this.root, value);
+	//easy case: no children -- delete node.
+	//medium case: 1 child -- splice out node, replace with the child
+	//hard case: 2 children -- start from k, and compute k's predecessor or successor, then swap the nodes, then delete new leaf node using existing method (for 1 or 0 children)
+	//running time based on height of the tree
+	//Wrapper to oversee the recursive removal and return of a node from the tree
+	delete(value){
+		return this.findAndDelete(this.root, value);
 	}
 	
 	//Search for the node (recursively) and delete it
-	findAndDeleteNode(currNode, value){
+	findAndDelete(currNode, value){
 		
 		//make sure node exists
-		if (!currNode) {
+		if (!currNode){
 			return null;
 		}
 		
@@ -164,43 +183,87 @@ class BinarySearchTree {
 		if (value === currNode.value) {
 			currNode = this.deleteNode(currNode);
 		} else if (value < currNode.value) {
-			currNode.left = this.findAndDeleteNode(currNode.left, value);
+			currNode.left = this.findAndDelete(currNode.left, value);
 		} else {
-			currNode.right = this.findAndDeleteNode(currNode.right, value);
+			currNode.right = this.findAndDelete(currNode.right, value);
 		}
 		
 		//return the updated root
 		return currNode;
 	}
 	
-	//Delete a node from the tree
+	//Delete a given node from the tree
 	deleteNode(node){
 	
-		//node has no children
-		if (node.left === null && node.right === null) {
+		//if no children, just need to set its position to null value
+		if (!node.left && !node.right) {
             return null;
         }
 		
-		//if children exist, recurse and reset the node in the tree
-		else if (node.left !== null && node.right !== null) {
+		//if both children exist, recurse and swap the node with its predecessor
+		else if (node.left && node.right) {
 
-			const bottomNode = this.getBottomRightLeaf(node.left);
-            const bottomNodeValue = bottomNode.value;
-
-            node = this.findAndDeleteNode(node, bottomNodeValue);
-            node.value = bottomNodeValue;
-
+			const predecessor = this.getBottomRightLeaf(node.left);
+			
+			//the predecessor's spot can be deleted, then re-assign node to predecessor's value
+			node = this.findAndDelete(node, predecessor.value);	 
+            node.value = predecessor.value; 
+			
             return node;
         } 
 		
-		//if just the left child exists, it will replace the removed node
-		else if (node.left !== null) {
+		//if just a left child exists, it will replace the removed node
+		else if (node.left) {
             return node.left;
         }
 
 		//otherwise if just the right child exists, it will replace the removed node
         return node.right;
 	}
+	
+	//Alternative search and delete approach...wrapper for deleteRec()
+	delete2(value){
+		return this.searchAndRemove(this.root, value);  //returning the deleted node
+	}
+		
+	//Alternate recursive search and delete, takes a starting point and a target search value
+	searchAndRemove(root, value){
+		
+		//console.log("currRoot, value: " + root.value, value);
+		
+		//base case -- the tree is empty
+		if (!root){
+			return root;
+		}
+		
+		//otherwise, recurse down the tree
+		if (value < root.value){
+			root.left = this.searchAndRemove(root.left, value);
+		} else if (value > root.value){
+			root.right = this.searchAndRemove(root.right, value);
+		}
+		
+		//this must be the target node, now it must be deleted
+		else {
+		
+			//node has one child or no child
+			if (!root.left){
+				return root.right;
+			} 
+			else if (!root.right){
+				return root.left;
+			}
+			
+			//node has two children, replace node with its inorder successor (smallest in the right subtree)
+			root.value = this.getSuccessor(root);
+  
+			//find and delete the inorder successor position, tracing the way back, reattach to right child
+			root.right = this.searchAndRemove(root.right, root.value);
+		}
+  
+		return root;
+	}
+
 	
 	//BFS - breadth first search - start from the top (root) and print horizontal levels going downward
 	bfs(){
@@ -257,45 +320,189 @@ class BinarySearchTree {
 		
 	}
 		 
-	//Helper to return the lowest right leaf from a given starting node
+	//Helper to return the lowest right leaf from a given starting node, AKA max element
 	getBottomRightLeaf(node) {
-	
+		
+		if (!node){
+			console.log("empty");
+			return null;
+		}
+		
 		//keep drilling to the right until can't anymore, then return what's found
-        let currentNode = node;
-        while (currentNode) {
-            if (currentNode.right === null) {
-                break;
-            }
-            currentNode = currentNode.right;
-        }
-        return currentNode;
+        let currNode = node;
+		
+		while (currNode.right){
+			currNode = currNode.right;
+		}
+		
+		return currNode;
+		
     }
+	
+	//Return the max element
+ 	getMax(node = this.root) {
+		
+		if (!node){
+			console.log("empty");
+			return null;
+		}
+		
+		let currNode = node;
+		
+		while (currNode.right) {
+			currNode = currNode.right;
+		}
+		
+		return currNode.value;
+		
+	} 
+	
+	//Return the min element
+	getMin() {
+		
+		if (!this.root){
+			console.log("empty tree");
+			return null;
+		}
+		
+		let currNode = this.root;
+		
+		while (currNode.left) {
+			currNode = currNode.left;
+		}
+		
+		return currNode.value;
+		
+	}
+	
+	//return the min value of a subtree starting from a given root 
+	minValue(root = this.root) {
+	
+		let min = root.value;
+		
+		while (root.left) {
+			min = root.left.value;
+			root = root.left;
+		}
+		
+		return min;
+	}
+	
+	//Return the next smallest element 
+	//if left subtree nonempty, return max key in left subtree
+	//otherwise follow parents pointers until find a smaller value..."left turn"
+	//from starting point k, go to left child, then go right until can't anymore
+	getPredecessor(root = this.root){
+		
+		if (!root.left) {
+			console.log("No predecessor available");
+			return null;
+		}
+		
+		root = root.left;
+		
+		while (root.right){
+			root = root.right;
+		}
+		
+		return root.value
+	}
+	
+	//Return the next largerst element, AKA the minimun element from the right subtree
+	getSuccessor(root = this.root){
+		
+		//making sure path available
+		if (!root.right) {
+			console.log("No successor available");
+			return null;
+		}
+		
+		root = root.right;
+		
+		while (root.left) {
+			root = root.left;
+		}
+		
+		return root.value;
+	}
+	
+	//given an order statistic i, return ith smallest key in the tree.
+	select(i){
+	
+	}
+	
+	//given a key value, return number of keys in the tree <= that value
+	rank(){
+	
+	}
 	
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Driver
 
-/*              (5)
- *            /    \
- *         (2)     (7)
- *        /  \     /  \
- *      (1) (3)  (6)  (8)       */ 
+/* Graph 1     (5)			Graph 2     (8)   
+             /    \	                   /   \
+          (2)     (7)                (3)    (10)
+         /  \     /  \              /  \       \
+       (1) (3)  (6)  (8)       	 (1)  (6)     (14)		
+									  / \     /
+									(4) (7) (13)		*/
 const bst = new BinarySearchTree();
 
 //Populating Tree
-bst.insert(5);
+/* bst.insert(5);
 bst.insert(2);
 bst.insert(3);
 bst.insert(1);
 bst.insert(7);
 bst.insert(6);
-bst.insert(8);
+bst.insert(8); */
 
-//Searching
+bst.insert(8);
+bst.insert(3);
+bst.insert(10);
+bst.insert(1);
+bst.insert(6);
+bst.insert(14);
+bst.insert(4);
+bst.insert(7);
+bst.insert(13);
+
+console.log("Getting min/max: ");
+console.log("min: " + bst.getMin());  //
+console.log("max: " + bst.getMax());  //
+console.log("minValue: " + bst.minValue());  //
+
+console.log("Getting Predecessor: ");
+console.log(bst.getPredecessor());  
+
+console.log("Getting Successor: ");
+console.log(bst.getSuccessor());  
+
+console.log("Traversing in-order...");  
+bst.inOrder(bst.root);		//1, 2, 3, 5, 6, 7, 8
+
+//Removing node
+console.log("Traversing pre-order...");  
+bst.preOrder(bst.root);		//
+
+let delNode = 6;
+console.log("Removing node " + delNode);  
+
+console.log("delete, new Root: " + bst.delete(delNode).value);  
+//console.log("delete2 Root: " + bst.delete2(delNode).value);  
+
+/* console.log("inOrder Tree after removal: ");  //8, 7, 6, 5, 3, 2, 1
+bst.inOrder(bst.root);  */
+
+console.log("Traversing pre-order...");  
+bst.preOrder(bst.root);		//
+
+/* //Searching
 let target = 5;
 console.log("Searching for target: " + target);
-console.log(bst.search(5));  //returns found node
+console.log(bst.search(target));  //returns found node
 
 //Getting bottom right leaf
 console.log("Successor right leaf: ");  
@@ -324,16 +531,13 @@ bst.dfs();		//5, 7, 8, 6, 2, 3, 1
 //Depth first search - recursive
 console.log("Depth first searching - recursive...");  
 bst.dfsRecursive(bst.root); 	//5, 2, 1, 3, 7, 6, 8
+ */
 
-//Removing node
-console.log("Removing node...");  
-bst.remove(2);
-console.log("Tree after removal: ");  //8, 7, 6, 5, 3, 2, 1
-bst.inOrder(bst.root);
 
+/* 
 //Inverting tree
 console.log("Inverting tree...");  //8, 7, 6, 5, 3, 2, 1
 bst.invert(bst.root);
 console.log("Tree after inversion: ");  //8, 7, 6, 5, 3, 1
 bst.inOrder(bst.root);
-
+ */
--- DataStructures/Queue.js
+++ DataStructures/Queue.js
@@ -22,6 +22,6 @@ class Queue extends Array {
 
 }
 
-//export { Queue };
-module.exports = { Queue };
+export { Queue };
+//module.exports = { Queue };
 
