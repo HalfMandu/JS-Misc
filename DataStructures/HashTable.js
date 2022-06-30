7dba896e80a33dd0f8cbb18bb0c1fcad89405d7d
Stephen Rinkus <jasperwednesday@gmail.com>
2022-06-30 16:13:00 -0700

    updating methods for es6

--- DataStructures/HashTable.js
+++ DataStructures/HashTable.js
@@ -2,13 +2,12 @@
 	HashTable -- basic chaining example
 	Stephen Rinkus
 	
-	HashTable implementation which uses an array to store key/val pairs
+	HashTable implementation which uses a 2D array to store key/val pairs
 	Uses chaining to resolve collisions
 	Hashing function is the sum of the ASCII codes of the chars of the key
-	127 buckets
 	
-	methods
-		_hash(key)
+	Methods
+		hashFunc(key)
 		set(key, value) 
 		get(key)
 		remove(key)
@@ -18,94 +17,213 @@
 
 class HashTable {
 	
-	//127 buckets
-	constructor() {
-		this.table = new Array(127);
-		this.size = 0;
+	//Defauts to 127 buckets
+	constructor(buckets = 127) {
+		this.table = new Array(buckets);
+		this.size = 0;		//total number of stored elements
 	}
-  
+	
 	//Hashing function to derive index from a key
-	_hash(key) {
+	hashFunc(key) {
 		
 		let hash = 0;
 		
 		//sum the ASCII code of the characters in the key
-		for (let i = 0; i < key.length; i++) {
-			hash += key.charCodeAt(i);
+		for (let currChar of [...key]) {
+			hash += currChar.charCodeAt(0);
 		}
-		
-		//to ensure that the hash value doesn't exceed the bucket size, need to use modulo
+				
+		//ensure that the hash value doesn't exceed the bucket size AKA array slots
 		return hash % this.table.length;
 
 	}
 	
-	//Set a key/value pair in the HashTable
+	//Set a key/value pair in the HashTable -- chaining
 	set(key, value) {
 	
-		const index = this._hash(key);
+		const index = this.hashFunc(key);	//1st dimension -- hash index
+		const chain = this.table[index];	//2nd dimension	-- chain
 		
-		//ensuring something has been set with this derived hash index before (not necessarily input value)
-		if (this.table[index]) {
+		//check if something has been set with this derived hash index before (not necessarily the input key)
+		if (chain) {
 		
-			//step through the chain of pairs located at the index, looking for the target key/val
-			for (let i = 0; i < this.table[index].length; i++) {
-				if (this.table[index][i][0] === key) {
-					this.table[index][i][1] = value;
-					return;
-				}
-			}
+			//index exists, now checking if key exists						
+ 			const keyLoc = chain.findIndex(keyVal => keyVal[0] == key);
 			
-			//hash index exists, but this key not in the chain...push the new key/value pair to this index
-			this.table[index].push([key, value]);
-			
+			//if key exists, overwrite it
+			if (keyLoc > -1){
+				chain[keyLoc][1] = value;
+				return;
+			} 
 		} 
 		//derived index hasn't been used yet, intialize it with second dimension array...
 		else {
 			this.table[index] = [];
-			this.table[index].push([key, value]);
 		}
+		
+		//hash index must exist, push the new pair
+		this.table[index].push([key, value]);	
 		this.size++;
 	}
-
 	
 	//Get a value from the HashTable
 	get(key) {
 	
-		const target = this._hash(key);
+		const index = this.hashFunc(key);
+		const chain = this.table[index];
 		
-		//check the chain at the derived index and return the correct key/value pair if found
-		if (this.table[target]) {
-			for (let i = 0; i < this.table.length; i++) {
-				if (this.table[target][i][0] === key) {
-					return this.table[target][i][1];
-				}
-			}
+		//check the chain at the derived index and return the correct key/value pair if key found
+		if (chain) {
+			return chain.find(keyVal => keyVal[0] === key)[1];
 		}
 		
 		return undefined;  //key not present
 	}
 
-
 	//Delete a key/value pair from the Hash Table
 	remove(key) {
 	
-		const index = this._hash(key);
+		const index = this.hashFunc(key);		
+		const chain = this.table[index];	
 		
-		//step through the second-level chain and remove the targeted key/value
-		if (this.table[index] && this.table[index].length) {
-			for (let i = 0; i < this.table.length; i++) {
-				if (this.table[index][i][0] === key) {
-					this.table[index].splice(i, 1);
-					this.size--;
-					return true;
-				}
-			}
+		if (chain) {
+			
+			//identify location of the key within the second-level chain
+			const keyLoc = chain.findIndex(keyVal => keyVal[0] == key);
+			
+			//if key is present, chop it out
+			if (keyLoc > -1){
+				chain.splice(keyLoc, 1);
+				this.size--;
+				return true;
+			} 
+			
 		} else {
 			return false;
 		}
 	}
 	
-	//display all key/value pairs stored in the HashTable
+	//Display all key/value pairs stored in the HashTable
+	display() {
+		
+		console.log("Hash Table: ");
+		console.log("Size: " + this.size);
+		console.log("------------------------------------------");
+		
+		this.table.forEach((values, index) => {
+			const chainedValues = values.map(
+				([key, value]) => `[ ${key}: ${value} ]`
+			);
+			console.log(`${index}: ${chainedValues}`);
+		});
+	
+		console.log("------------------------------------------");
+	
+	}
+	
+	//Resize hashtable
+	resize(){
+		
+	}
+	
+	//Alternative hash function
+	hash2(key) {
+		return key.toString().length % this.size;
+	}
+
+}
+
+
+		
+	//Defauts to 127 buckets
+	constructor(buckets = 127) {
+		this.table = new Array(buckets);
+		this.size = 0;		//total number of stored elements
+	}
+	
+	//Hashing function to derive index from a key
+	hashFunc(key) {
+		
+		let hash = 0;
+		
+		//sum the ASCII code of the characters in the key
+		for (let currChar of [...key]) {
+			hash += currChar.charCodeAt(0);
+		}
+				
+		//ensure that the hash value doesn't exceed the bucket size AKA array slots
+		return hash % this.table.length;
+
+	}
+	
+	//Set a key/value pair in the HashTable -- chaining
+	set(key, value) {
+	
+		const index = this.hashFunc(key);	//1st dimension -- hash index
+		const chain = this.table[index];	//2nd dimension	-- chain
+		
+		//check if something has been set with this derived hash index before (not necessarily the input key)
+		if (chain) {
+		
+			//index exists, now checking if key exists						
+ 			const keyLoc = chain.findIndex(keyVal => keyVal[0] == key);
+			
+			//if key exists, overwrite it
+			if (keyLoc > -1){
+				chain[keyLoc][1] = value;
+				return;
+			} 
+									
+			//hash index exists, but this key not in the chain...push the new key/value pair to this index
+			chain.push([key, value]);
+			
+		} 
+		//derived index hasn't been used yet, intialize/add it with second dimension array...
+		else {
+			this.table[index] = [];
+			this.table[index].push([key, value]);	
+		}
+		this.size++;
+	}
+	
+	//Get a value from the HashTable
+	get(key) {
+	
+		const index = this.hashFunc(key);
+		const chain = this.table[index];
+		
+		//check the chain at the derived index and return the correct key/value pair if key found
+		if (chain) {
+			return chain.find(keyVal => keyVal[0] === key)[1];
+		}
+		
+		return undefined;  //key not present
+	}
+
+	//Delete a key/value pair from the Hash Table
+	remove(key) {
+	
+		const index = this.hashFunc(key);		
+		const chain = this.table[index];	
+		
+		if (chain) {
+			
+			//identify location of the key within the second-level chain
+			const keyLoc = chain.findIndex(keyVal => keyVal[0] == key);
+			
+			//if key is present, chop it out
+			if (keyLoc > -1){
+				chain.splice(keyLoc, 1);
+				this.size--;
+				return true;
+			} 
+			
+		} else {
+			return false;
+		}
+	}
+	
+	//Display all key/value pairs stored in the HashTable
 	display() {
 		
 		console.log("Hash Table: ");
